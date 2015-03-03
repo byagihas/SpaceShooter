91b9746c3741c8f609251244a4997b979c515bbf
yagihashib <yagihashib@gmail.com>
2015-03-03 18:23:05 -0500

    Fixed Indentation

--- game.js
+++ game.js
@@ -2,45 +2,31 @@
 //                                                           //
 //                    CONSTANT STATE                         //
 
-// TODO: DECLARE and INTIALIZE your constants here
 var START_TIME = currentTime();
-
 var BACKGROUND = loadImage("gameBackground.jpg");
 var PLAYER_IMAGE = loadImage("player.png");
 var BULLET_IMAGE = loadImage("bullet.png");
 var ENEMY_IMAGE = loadImage("enemy.png");
 var EXPLOSION_IMAGE = loadImage("explosion.png");
 var ENEMY_BULLET_IMAGE = loadImage("enemybullet.png");
-
 var BULLET_SOUND = loadSound("bulletsound.wav");
 var EXPLOSION_SOUND = loadSound("explosion.wav");
-
 var BULLET_SPEED = 700;
 var PLAYER_SPEED = 1000;
 var ENEMY_SPEED = 30;
-
-
-
-
 
 ///////////////////////////////////////////////////////////////
 //                                                           //
 //                     MUTABLE STATE                         //
 
-// TODO: DECLARE your variables here
 var lastKeyCode;
-
-
 var player;
 var enemies;
 var enemyBullets;
 var bullets;
 var explosion;
-
 var resetFlag = false;
-
 var clearBonus = false;
-
 var Score = 0;
 
 ///////////////////////////////////////////////////////////////
@@ -49,15 +35,11 @@ var Score = 0;
 defineGame("Space Invaders", "Brandon Yagihashi", "title.png");
 // When setup happens...
 function onSetup() {
-    // TODO: INITIALIZE your variables here
 
     lastKeyCode = 0;
-
-
     player = makeObject();
 
     player.image = PLAYER_IMAGE;
-
     player.width = 128;
     player.height = 128;
 
@@ -71,24 +53,16 @@ function onSetup() {
 
     player.leftKey     = asciiCode('A');
     player.leftActive  = false;
-
     player.rightKey    = asciiCode('D');
     player.rightActive = false;
-
     player.spaceBar = asciiCode(' ');
     player.barActive = false;
-
     player.q = asciiCode('Q');
-
-
     player.lastfire = 0;
-
-
 
     bullets = [];
     enemyBullets = [];
     enemies = [];
-
 
     var num= 0;
     setTimeout(function(){
@@ -108,39 +82,27 @@ function onSetup() {
     }
     }, 6000);
 
-
     setInterval(function(){
       buildEnemyBullets(enemies[randomInteger(0,length(enemies))].pos.x,enemies[randomInteger(0,length(enemies))].pos.y);
     }, 8000);
 
-
 }
-
-
-
 
 // When a key is pushed
 function onKeyStart(key) {
   var now = currentTime();
   var time =1.0/30.0;
     if (key == player.leftKey) {
-        player.leftActive = true;
-
-
+      player.leftActive = true;
     }
-
     if (key == player.rightKey) {
-        player.rightActive = true;
-
-
+      player.rightActive = true;
     }
     if(key == player.spaceBar){
       player.barActive = true;
       buildBullets(player.pos.x, player.pos.y - player.image.height/ 2);
       playSound(BULLET_SOUND);
       player.lastfire = now;
-
-
     }
     if(key == player.q){
       strokeLine(player.pos.x,player.pos.y, player.pos.x, player.pos.y - 1000, "white", 50);
@@ -149,13 +111,11 @@ function onKeyStart(key) {
 
 
 function onKeyEnd(key) {
-
     if (key == player.leftKey) {
-        player.leftActive = false;
+      player.leftActive = false;
     }
-
     if (key == player.rightKey) {
-        player.rightActive = false;
+      player.rightActive = false;
     }
     if(key == player.spaceBar){
       player.barActive = false;
@@ -165,7 +125,6 @@ function onKeyEnd(key) {
 // Called 30 times or more per second
 function onTick() {
     var time = 1.0/30.0;
-
     movePlayer(time);
     moveBullets(time);
     moveEnemies(time);
@@ -173,13 +132,11 @@ function onTick() {
     doCollisions(time);
     doPlayerColl();
     buildGraphics();
-
     if(resetFlag){
       reset();
       Score = 0;
       resetFlag = false;
     }
-
 }
 
 
@@ -189,96 +146,74 @@ function onTick() {
 //                      HELPER RULES                         //
 
 function movePlayer(time){
-
     if ((player.leftActive && ! player.rightActive) && (player.pos.x > 55)) {
         player.pos.x = player.pos.x - PLAYER_SPEED * time;
-    } else if ((! player.leftActive && player.rightActive) && (player.pos.x < screenWidth -55)) {
+    }
+    else if ((! player.leftActive && player.rightActive) && (player.pos.x < screenWidth -55)) {
         player.pos.x = player.pos.x + PLAYER_SPEED * time;
     }
-
-
 }
 
 function buildGraphics(){
-      var b = 0;
-      var bullet;
+    var b = 0;
+    var bullet;
+    var e = 0;
+    var enemy;
+    var eb=0;
+    var enemyBullet;
 
-      var e = 0;
-      var enemy;
+    clearRectangle(0, 0, screenWidth, screenHeight);
+    drawImage(BACKGROUND,0,0,screenWidth,screenHeight);
 
-      var eb=0;
-      var enemyBullet;
-
-      clearRectangle(0, 0, screenWidth, screenHeight);
-      drawImage(BACKGROUND,0,0,screenWidth,screenHeight);
-
-      while (b < length(bullets)) {
-          bullet = bullets[b];
-          drawposed(bullet);
-          b = b + 1;
-      }
-      while(e < length(enemies)){
-        enemy = enemies[e];
-        drawposed(enemy);
-        e = e+1;
-      }
-      while(eb < length(enemyBullets)){
-        enemyBullet = enemyBullets[eb];
-        drawposed(enemyBullet);
-        eb = eb+1;
-      }
-      fillText(Score, screenWidth/20, screenHeight/12,"white", "bold 64px Helvetica", "left", "top");
+    while (b < length(bullets)) {
+       bullet = bullets[b];
+       drawposed(bullet);
+       b = b + 1;
+    }
+    while(e < length(enemies)){
+      enemy = enemies[e];
+      drawposed(enemy);
+      e = e+1;
+    }
+    while(eb < length(enemyBullets)){
+      enemyBullet = enemyBullets[eb];
+      drawposed(enemyBullet);
+      eb = eb+1;
+    }
+    fillText(Score, screenWidth/20, screenHeight/12,"white", "bold 64px Helvetica", "left", "top");
       if(clearBonus){
         fillText("Clear Bonus + 1000!", screenWidth/15, screenHeight/12, "white", "bold 64px Helvetica", "left", "top");
-      }
-      drawposed(player);
-
-
-
-
+    }
+    drawposed(player);
 }
 
-
-
 function buildBullets(x,y){
-      var bullet;
-
-      bullet = makeObject();
-
-      bullet.image = BULLET_IMAGE;
-
-
-      bullet.pos = makeObject();
-      bullet.pos.x = x;
-      bullet.pos.y = y;
-
-      bullet.width = makeObject();
-      bullet.width = 35;
-
-      bullet.height = makeObject();
-      bullet.height = 35;
-
-      insertBack(bullets, bullet);
-
+    var bullet;
+    bullet = makeObject();
+    bullet.image = BULLET_IMAGE;
+    bullet.pos = makeObject();
+    bullet.pos.x = x;
+    bullet.pos.y = y;
+    bullet.width = makeObject();
+    bullet.width = 35;
+    bullet.height = makeObject();
+    bullet.height = 35;
+    insertBack(bullets, bullet);
 }
 
 function moveBullets(time){
     var b;
     var bullet;
-
     b = 0;
     while (b < length(bullets)) {
-        bullet = bullets[b];
-
+       bullet = bullets[b];
        bullet.pos.y = bullet.pos.y - BULLET_SPEED * time;
-
-        if (bullet.pos.y + bullet.image.height / 2 < 0) {
-
-            removeAt(bullets, b);
-        } else {
-
+       if (bullet.pos.y + bullet.image.height / 2 < 0) {
+          removeAt(bullets, b);
+       } 
+       else{
             b = b + 1;
-        }
+       }
     }
 }
 
@@ -300,8 +235,8 @@ function buildEnemy(x,y){
 
       enemy.isHit = false;
       insertBack(enemies, enemy);
-
 }
+
 function moveEnemies(time){
       var e;
       var enemy;
@@ -313,22 +248,16 @@ function moveEnemies(time){
             enemy.pos.x = enemy.pos.x + 5*time;
             enemy.pos.x = enemy.pos.x - 5*time;
         }, 5000)
-
         enemy.pos.y = enemy.pos.y + ENEMY_SPEED*time;
         e++;
       }
-
 }
 
 function buildEnemyBullets(x,y){
       var enemyB;
-
       enemyB = makeObject();
-
       enemyB.image = ENEMY_BULLET_IMAGE;
-
       enemyB.pos = makeObject();
-
       enemyB.pos.x = x;
       enemyB.pos.y = y;
 
@@ -341,25 +270,23 @@ function buildEnemyBullets(x,y){
       insertBack(enemyBullets, enemyB);
 
 }
+
 function moveEnemyBullets(time){
-  var eb;
-  var ebullet;
+    var eb;
+    var ebullet;
 
-  eb = 0;
-  while (eb < length(enemyBullets)) {
-     ebullet = enemyBullets[eb];
-     ebullet.pos.y = ebullet.pos.y + BULLET_SPEED * time;
-      if (ebullet.pos.y - ebullet.image.height / 2 < 0) {
-
-          removeAt(enemyBullets, eb);
-      } else {
-
-          eb = eb + 1;
-      }
-  }
-
+    eb = 0;
+    while (eb < length(enemyBullets)) {
+       ebullet = enemyBullets[eb];
+       ebullet.pos.y = ebullet.pos.y + BULLET_SPEED * time;
+       if (ebullet.pos.y - ebullet.image.height / 2 < 0) {
+         removeAt(enemyBullets, eb);
+       } 
+       else {
+         eb = eb + 1;
+       }
+    }
 }
-
 
 function drawposed(object){
     drawImage(object.image,
@@ -367,46 +294,33 @@ function drawposed(object){
               object.pos.y - object.image.height / 2);
 }
 
-
 function doCollisions() {
-
     var b;
     var e;
     for(i=0;i<length(enemies);i++){
       for(k=0;k<length(bullets);k++){
         if(collides(enemies[i],bullets[k])){
-
           enemies[i].image = EXPLOSION_IMAGE;
           playSound(EXPLOSION_SOUND);
-
           b = k;
           e = i;
           removeAt(bullets, b);
           setTimeout(function(){removeAt(enemies, e)}, 100);
           Score += 200;
-
-
         }
-
       }
     }
-
 }
 
 function doPlayerColl(){
-
     for(i=0;i<length(enemyBullets);i++){
       if(collides(player,enemyBullets[i]) || collides(player, enemies[i])){
         player.image = EXPLOSION_IMAGE;
         playSound(EXPLOSION_SOUND);
         resetFlag = true;
-
-
       }
     }
-
 }
-
 
 function collides(obj1,obj2){
     if (obj1.pos.x < obj2.pos.x + obj2.width &&
@@ -415,5 +329,4 @@ function collides(obj1,obj2){
        obj1.height + obj1.pos.y > obj2.pos.y) {
         return true;
     }
-
 }
